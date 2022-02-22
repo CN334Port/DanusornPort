044ee2376401bceb00a4164243590b1ca267e82c
Danusorn6643 <mannakka12917@gmail.com>
2022-02-22 19:28:20 +0700

    login adminlogin register

--- portDanusorn/resources/views/auth/forgot-password.blade.php
+++ portDanusorn/resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,138 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Kodinger">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>My Login Page</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/my-login.css">
+</head>
+<style>
+    html,body {
+	height: 100%;
+    }
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+    body.my-login-page {
+        background-color: #f7f9fb;
+        background: url(../../assets/img/24.jpg);
+        font-size: 14px;
+    }
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    .my-login-page .brand {
+        width: 90px;
+        height: 90px;
+        overflow: hidden;
+        border-radius: 50%;
+        margin: 40px auto;
+        box-shadow: 0 4px 8px rgba(0,0,0,.05);
+        position: relative;
+        z-index: 1;
+    }
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+    .my-login-page .brand img {
+        width: 100%;
+    }
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+    .my-login-page .card-wrapper {
+        width: 400px;
+    }
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+    .my-login-page .card {
+        border-color: transparent;
+        box-shadow: 0 4px 8px rgba(0,0,0,.05);
+    }
 
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+    .my-login-page .card.fat {
+        padding: 10px;
+    }
+
+    .my-login-page .card .card-title {
+        margin-bottom: 30px;
+    }
+
+    .my-login-page .form-control {
+        border-width: 2.3px;
+    }
+
+    .my-login-page .form-group label {
+        width: 100%;
+    }
+
+    .my-login-page .btn.btn-block {
+        padding: 12px 10px;
+    }
+
+    .my-login-page .footer {
+        margin: 40px 0;
+        color: #888;
+        text-align: center;
+    }
+
+    @media screen and (max-width: 425px) {
+        .my-login-page .card-wrapper {
+            width: 90%;
+            margin: 0 auto;
+        }
+    }
+
+    @media screen and (max-width: 320px) {
+        .my-login-page .card.fat {
+            padding: 0;
+        }
+
+        .my-login-page .card.fat .card-body {
+            padding: 15px;
+        }
+    }
+</style>
+<body class="my-login-page">
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-md-center h-100">
+				<div class="card-wrapper">
+					<div class="brand">
+                        <img src="/assets/img/logo3.png" alt="logo">
+					</div>
+					<div class="card fat">
+						<div class="card-body">
+							<h4 class="card-title">Forgot Password</h4>
+                            <form method="POST" action="{{ route('password.email') }}">
+                            @csrf
+							<form method="POST" class="my-login-validation" novalidate="">
+                                <x-auth-session-status class="mb-4" :status="session('status')" />
+                                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                                    
+                                <section>
+                                    <p>Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+                                </section>
+								<div class="form-group">
+                                    <x-label for="email" :value="__('Email')" />
+                                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+								</div>
+
+								<div class="form-group m-0">
+									<button type="submit" class="btn btn-primary btn-block" href="{{route('login')}}">
+                                        Email Password Reset Link
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="footer">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="js/my-login.js"></script>
+</body>
+</html>
 </x-guest-layout>
--- portDanusorn/resources/views/auth/login.blade.php
+++ portDanusorn/resources/views/auth/login.blade.php
@@ -1,56 +1,167 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Kodinger">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>My Login Page</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/my-login.css">
+</head>
+<style>
+    html,body {
+	height: 100%;
+    }
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    body.my-login-page {
+        background-color: #f7f9fb;
+        background: url(../../assets/img/24.jpg);
+        font-size: 14px;
+    }
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    .my-login-page .brand {
+        width: 90px;
+        height: 90px;
+        overflow: hidden;
+        border-radius: 50%;
+        margin: 40px auto;
+        box-shadow: 0 4px 8px rgba(0,0,0,.05);
+        position: relative;
+        z-index: 1;
+    }
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+    .my-login-page .brand img {
+        width: 100%;
+    }
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+    .my-login-page .card-wrapper {
+        width: 400px;
+    }
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+    .my-login-page .card {
+        border-color: transparent;
+        box-shadow: 0 4px 8px rgba(0,0,0,.05);
+    }
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+    .my-login-page .card.fat {
+        padding: 10px;
+    }
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+    .my-login-page .card .card-title {
+        margin-bottom: 30px;
+    }
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+    .my-login-page .form-control {
+        border-width: 2.3px;
+    }
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+    .my-login-page .form-group label {
+        width: 100%;
+    }
+
+    .my-login-page .btn.btn-block {
+        padding: 12px 10px;
+    }
+
+    .my-login-page .footer {
+        margin: 40px 0;
+        color: #888;
+        text-align: center;
+    }
+
+    @media screen and (max-width: 425px) {
+        .my-login-page .card-wrapper {
+            width: 90%;
+            margin: 0 auto;
+        }
+    }
+
+    @media screen and (max-width: 320px) {
+        .my-login-page .card.fat {
+            padding: 0;
+        }
+
+        .my-login-page .card.fat .card-body {
+            padding: 15px;
+        }
+    }
+</style>
+<body class="my-login-page">
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-md-center h-100">
+				<div class="card-wrapper">
+					<div class="brand">
+                        <img src="/assets/img/logo3.png" alt="logo">
+					</div>
+					<div class="card fat">
+						<div class="card-body">
+							<h4 class="card-title">Login</h4>
+                            <form method="POST" action="{{ route('login') }}">
+                            @csrf
+							<form method="POST" class="my-login-validation" novalidate="">
+                                <x-auth-session-status class="mb-4" :status="session('status')" />
+                                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                                    <h2 class="sr-only">Login Form</h2>
+								<div class="form-group">
+                                    <x-label for="email" :value="__('Email')" />
+                                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+									<div class="invalid-feedback">
+										Email is invalid
+									</div>
+								</div>
+
+								<div class="form-group">
+                                <x-label for="password" :value="__('Password')" />
+                                    <x-input id="password" class="block mt-1 w-full"
+                                            type="password"
+                                            name="password"
+                                            required autocomplete="current-password" />
+                                
+                                @if (Route::has('password.request'))
+                                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
+                                @endif
+								    <div class="invalid-feedback">
+								    	Password is required
+							    	</div>
+								</div>
+
+								<div class="form-group">
+									<div class="custom-checkbox custom-control">
+                                    <label for="remember_me" class="inline-flex items-center">
+                                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                    </label>
+									</div>
+								</div>
+
+								<div class="form-group m-0">
+									<button type="submit" class="btn btn-primary btn-block" href="{{route('dashboard')}}">
+										Login
+									</button>
+								</div>
+								<div class="mt-4 text-center">
+									Don't have an account? <a href="{{ route('register') }}">Create One</a>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="footer">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="js/my-login.js"></script>
+</body>
+</html>
 </x-guest-layout>
